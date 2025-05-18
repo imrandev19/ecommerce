24412244a01919da37cd4aa666ddfb953941849e
Md. Imran Hossain <imrandev19@gmail.com>
2025-05-18 20:17:49 +0600

    cart functional

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,4 +1,18 @@
+const cartModel = require("../model/cartModel");
+
 const addtoCartController = async (req,res)=>{
-    res.send("I am add to cart")
+    try {
+        let {product, variant, user, quantity} = req.body
+        const addCart = await cartModel({
+            product, variant, user, quantity
+        })
+
+        addCart.save()
+return res
+      .status(200)
+      .json({ success: true, message: "Cart Added Successfully", data:addCart });
+    } catch (error) {
+        return res.status(500).json({ success: false, message: error.message });
+    }
 }
 module.exports = addtoCartController

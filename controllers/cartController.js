51a4e06001146d91da395e73f63804ea1f5f71e7
Md. Imran Hossain <imrandev19@gmail.com>
2025-05-21 22:27:07 +0600

    fetch cart

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,18 +1,43 @@
 const cartModel = require("../model/cartModel");
 
-const addtoCartController = async (req,res)=>{
-    try {
-        let {product, variant, user, quantity} = req.body
-        const addCart = await cartModel({
-            product, variant, user, quantity
-        })
+const addtoCartController = async (req, res) => {
+  try {
+    let { product, variant, user, quantity } = req.body;
+    const addCart = await cartModel({
+      product,
+      variant,
+      user,
+      quantity,
+    });
 
-        addCart.save()
-return res
+    addCart.save();
+    return res
       .status(200)
-      .json({ success: true, message: "Cart Added Successfully", data:addCart });
-    } catch (error) {
-        return res.status(500).json({ success: false, message: error.message });
+      .json({
+        success: true,
+        message: "Cart Added Successfully",
+        data: addCart,
+      });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+let getCartController = async (req, res) => {
+  try {
+    let { id } = req.params;
+    const getcart = await cartModel
+      .find({ user: id })
+      .populate("variant product user");
+    if (getcart.length == 0) {
+      return res.status(200).json({ success: true, message: "Cart is Empty" });
     }
-}
-module.exports = addtoCartController
+    return res
+      .status(200)
+      .json({ success: true, message: "Cart found", data: getcart });
+  } catch (error) {
+    return res.status(500).json({ success: false, message: error.message });
+  }
+};
+
+module.exports = { addtoCartController, getCartController };
--- router/api/cart.js
+++ router/api/cart.js
@@ -1,6 +1,8 @@
 const express = require("express")
-const addtoCartController = require("../../controllers/cartController")
+const {addtoCartController, getCartController} = require("../../controllers/cartController")
 const router = express.Router()
 // http://localhost:4000/api/cart/addtocart
 router.post("/addtocart", addtoCartController)
+// http://localhost:4000/api/cart/getcartitems
+router.get("/getcartitems/:id", getCartController)
 module.exports = router
